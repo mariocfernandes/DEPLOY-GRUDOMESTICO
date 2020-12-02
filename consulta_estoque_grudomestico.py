8200a7bf4c13cf739b7f02c346374d729dcfa6b9
MARIO CESAR FERNANDES <mariocf0709@gmail.com>
2020-12-02 17:38:24 -0300

    Update consulta_estoque_grudomestico.py

--- consulta_estoque_grudomestico.py
+++ consulta_estoque_grudomestico.py
@@ -3,10 +3,10 @@ import streamlit as st
 
 # Titulo
 st.title('Consulta Estoque GRU-DOMESTICO')
-st.header('Estoques em 13-11-2020')
+st.header('Estoques em 02-12-2020')
 
 
-arquivo = 'gru13112020.csv'
+arquivo = 'gru02122020.csv'
 
 @st.cache
 def get_data():
